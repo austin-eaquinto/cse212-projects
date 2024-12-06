f45f6e99dd721ae8a2dd428b67f7a185d3d6fc7a
Austin Eaquinto <austin.eaquinto@gmail.com>
2024-12-06 16:50:07 -0700

    w2 code

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,86 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: The Enqueue function shall add an item (which contains both data and priority) to the back of the queue.
+    // Expected Result: Add and item to the queue in the correct order of FIFO
+    // Defect(s) Found: none
     public void TestPriorityQueue_1()
     {
+        
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        
+        priorityQueue.Enqueue("tim", 1);
+        priorityQueue.Enqueue("bill", 3);
+        priorityQueue.Enqueue("rob", 2);
+
+        // Assert.Fail("Implement the test case and then remove this.");
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: The Dequeue function shall remove the item with the highest priority and return its value.
+    // Expected Result: The item with the highest priority is removed first
+    // Defect(s) Found: none
     public void TestPriorityQueue_2()
     {
+        
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("tim", 1);
+        priorityQueue.Enqueue("bill", 3);
+        priorityQueue.Enqueue("rob", 2);
+
+        var person = priorityQueue.Dequeue();
+
+        Assert.AreEqual(person, "bill");
+
+        // Assert.Fail("Implement the test case and then remove this.");
     }
 
     // Add more test cases as needed below.
+
+    [TestMethod]
+    // Scenario: If there are more than one item with the highest priority, then the item closest to the front of the 
+                // queue will be removed and its value returned.
+    // Expected Result: If two items have the same priority, the one that was first in will be first out
+    // Defect(s) Found: none
+    public void TestPriorityQueue_3()
+    {
+        
+        var priorityQueue = new PriorityQueue();
+
+        priorityQueue.Enqueue("tim", 1);
+        priorityQueue.Enqueue("bill", 3);
+        priorityQueue.Enqueue("rob", 2);
+        priorityQueue.Enqueue("daniel", 3);
+
+        var person = priorityQueue.Dequeue();
+
+        Assert.AreEqual(person, "bill");
+
+        // Assert.Fail("Implement the test case and then remove this.");
+    }
+
+    [TestMethod]
+    // Scenario: If the queue is empty, then an error exception shall be thrown.
+    // Expected Result: return an error message if the queue is empty
+    // Defect(s) Found: none
+    public void TestPriorityQueue_4()
+    {
+        try
+        {
+            var priorityQueue = new PriorityQueue();
+            priorityQueue.Enqueue("tim", 1);
+            priorityQueue.Enqueue("bill", 3);
+            priorityQueue.Enqueue("rob", 2);
+            priorityQueue.Enqueue("daniel", 3);
+
+            var person = priorityQueue.Dequeue();
+        }
+        catch (InvalidOperationException)
+        {
+            Console.WriteLine("The queue is empty");
+        }
+
+        // Assert.Fail("Implement the test case and then remove this.");
+    }
 }
--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -3,13 +3,13 @@ public class Translator
     public static void Run()
     {
         var englishToGerman = new Translator();
-        englishToGerman.AddWord("House", "Haus");
+        englishToGerman.AddWord("House", "Haus"); // add to the dictionary
         englishToGerman.AddWord("Car", "Auto");
         englishToGerman.AddWord("Plane", "Flugzeug");
         Console.WriteLine(englishToGerman.Translate("Car")); // Auto
         Console.WriteLine(englishToGerman.Translate("Plane")); // Flugzeug
         Console.WriteLine(englishToGerman.Translate("Train")); // ???
-    }
+    }                                               // Train is not in the dictionary
 
     private Dictionary<string, string> _words = new();
 
@@ -22,9 +22,10 @@ public class Translator
     /// <param name="fromWord">The word to translate from</param>
     /// <param name="toWord">The word to translate to</param>
     /// <returns>fixed array of divisors</returns>
-    public void AddWord(string fromWord, string toWord)
+    public void AddWord(string fromWord, string toWord) // (key, value)
     {
         // ADD YOUR CODE HERE
+        _words[fromWord] = toWord; // Dictionary[key] = value;
     }
 
     /// <summary>
@@ -35,6 +36,13 @@ public class Translator
     public string Translate(string fromWord)
     {
         // ADD YOUR CODE HERE
-        return "";
-    }
+        string newWord = "???"; // gives the required value to newWord
+
+            if (_words.ContainsKey(fromWord)) // if the Dictionary does contain the key (fromWord)
+            {
+                newWord = _words[fromWord]; // then newWord = Dictionary[key]
+            }
+        return newWord; // returns either "???" or _words[fromWord]
+    } // because there is no user input and the code is already set in the Run method when it comes to 
+      // the 'trains' word (key) it prints "???" as the value
 }
